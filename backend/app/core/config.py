69fff9ed464463d1ba602ac0398d2fcad499d01a
halmetov <ithubsayramm@gmail.com>
2025-11-19 18:28:51 +0500

    fix

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,14 +3,18 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    database_url: str = "sqlite+aiosqlite:///./app.db"
-    jwt_secret_key: str = "super-secret"
+    database_url: str = "postgresql+asyncpg://postgres:123@localhost:5432/kassa_db"
+
+    jwt_secret_key: str = "kassa_project"
     jwt_algorithm: str = "HS256"
     access_token_expire_minutes: int = 60 * 24
     refresh_token_expire_minutes: int = 60 * 24 * 7
     media_root: str = "app/static/uploads"
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+    )
 
 
 @lru_cache(maxsize=1)
