81471b263557741b919ae4426c0790959a456d11
halmetov <ithubsayramm@gmail.com>
2025-12-24 15:33:16 +0500

    config

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -60,6 +60,7 @@ class Settings(BaseSettings):
             "http://127.0.0.1:8080",
             "http://localhost:5173",
             "http://127.0.0.1:5173",
+            "https://samen.inbrain.kz",
         ]
 
     @property
